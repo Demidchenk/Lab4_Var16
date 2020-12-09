eef32fcbb67a41f345d5276d11030bc382b38ac7
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-12-09 09:44:54 +0200

    Add project files.

---
+++ Lab4_Var16/Lab4_Var16.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x = 1, y = 1, s = 0;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        s += x / (1 + abs(y));
+        y += x;
+        x *= 0.3;
+    }
+    cout << s;
+    return 0;
+}
